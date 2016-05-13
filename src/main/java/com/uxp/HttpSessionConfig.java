6146ac376a1a6d5d1a31e2717762d0c6f2b3b6be
pyromaticx <krginvest@gmail.com>
2016-05-13 12:00:14 -0700

    is

--- src/main/java/com/uxp/HttpSessionConfig.java
+++ src/main/java/com/uxp/HttpSessionConfig.java
@@ -3,6 +3,7 @@ package com.uxp;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
 
 @EnableRedisHttpSession 
 public class HttpSessionConfig {
@@ -11,4 +12,11 @@ public class HttpSessionConfig {
         public JedisConnectionFactory connectionFactory() {
                 return new JedisConnectionFactory(); 
         }
+        
+        static class SessionInit extends AbstractHttpSessionApplicationInitializer { 
+
+            public SessionInit() {
+                    super(HttpSessionConfig.class); 
+            }
+    }
 }
