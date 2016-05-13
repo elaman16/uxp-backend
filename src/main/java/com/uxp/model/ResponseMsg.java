603189a8ab05fc23abb2c2da6799bfa69ce3d542
pyromaticx <krginvest@gmail.com>
2016-05-13 16:28:14 -0700

    serial

--- src/main/java/com/uxp/model/ResponseMsg.java
+++ src/main/java/com/uxp/model/ResponseMsg.java
@@ -1,6 +1,9 @@
 package com.uxp.model;
 
-public class ResponseMsg {
+import java.io.Serializable;
+
+public class ResponseMsg implements Serializable {
+	private static final long serialVersionUID = 1L;
 	private String title;
 	private String description;
 	
