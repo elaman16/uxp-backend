ca7deda2c1b3a50fb5669f99000bf9e3472255e5
pyromaticx <krginvest@gmail.com>
2016-05-20 13:27:18 -0700

    retry

--- src/main/java/com/uxp/Application.java
+++ src/main/java/com/uxp/Application.java
@@ -3,11 +3,8 @@ package com.uxp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.cloud.aws.context.config.annotation.EnableContextCredentials;
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
