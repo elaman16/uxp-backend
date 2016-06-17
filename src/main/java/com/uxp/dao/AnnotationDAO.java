82609b43201d8bc96306136ed94b2db2b276dde2
pyromaticx <krginvest@gmail.com>
2016-06-17 14:26:18 -0700

    sadf

--- src/main/java/com/uxp/dao/AnnotationDAO.java
+++ src/main/java/com/uxp/dao/AnnotationDAO.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.uxp.model.Annotation;
@@ -14,8 +15,8 @@ public interface AnnotationDAO extends CrudRepository<Annotation, Long> {
 
 	List<Annotation> findAllByUserName(String userName);
 	
-	@Query(value = "SELECT user_name FROM annotation WHERE user_name = ?1 LIMIT ?2, 10", nativeQuery = true)
-	List<Annotation> userAnnotationsPaged(String userName, Integer page);
+	@Query(value = "SELECT user_name FROM annotation WHERE user_name = :userName LIMIT :page, 10", nativeQuery = true)
+	List<Annotation> userAnnotationsPaged(@Param("userName") String userName, @Param("page") Integer page);
 	
 	@Query(value = "SELECT * FROM annotation LIMIT ?1, 10", nativeQuery = true)
 	List<Annotation> allAnnotationsPaged(Integer page);
