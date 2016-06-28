b70d339ecae723b84c8839121d4e26ffa20fe005
pyromaticx <krginvest@gmail.com>
2016-06-28 11:27:09 -0700

    garbage

--- src/main/java/com/uxp/controller/AnnotationController.java
+++ src/main/java/com/uxp/controller/AnnotationController.java
@@ -4,6 +4,7 @@ package com.uxp.controller;
 
 import java.security.Key;
 import java.util.Collections;
+import java.util.HashMap;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -134,7 +135,7 @@ public class AnnotationController {
 	public @ResponseBody Object getUserAnnotations(@PathVariable("userName") String userName, @RequestHeader(name="programId", required=false) String programId, @RequestHeader(name="page", defaultValue="0") Integer page, HttpServletRequest request, HttpServletResponse response, @RequestHeader(name="Authorization") String token) {	
 		System.out.println("HEAD FROM TOKEN: " + Jwts.parser().setSigningKey(key).parseClaimsJws(token).getHeader().toString());
 		System.out.println("BODY FROM TOKEN: " + Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody().toString());
-		User user = (User) Jwts.parser().setSigningKey(key).parseClaimsJws(token).getHeader().get("user");
+		User user = User.class.cast(Jwts.parser().setSigningKey(key).parseClaimsJws(token).getHeader().get("user"));
 		System.out.println("From encoded user class: " + user.getUserId());
 		try {
 			if(Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody().getIssuer().equals("UxP-Gll")) {
