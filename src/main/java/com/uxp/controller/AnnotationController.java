376cbae90fc51c5de3db75182718e620e57894f4
pyromaticx <krginvest@gmail.com>
2016-06-28 11:57:00 -0700

    tokenz

--- src/main/java/com/uxp/controller/AnnotationController.java
+++ src/main/java/com/uxp/controller/AnnotationController.java
@@ -137,9 +137,10 @@ public class AnnotationController {
 	public @ResponseBody Object getUserAnnotations(@PathVariable("userName") String userName, @RequestHeader(name="programId", required=false) String programId, @RequestHeader(name="page", defaultValue="0") Integer page, HttpServletRequest request, HttpServletResponse response, @RequestHeader(name="Authorization") String token) {	
 		System.out.println("HEAD FROM TOKEN: " + Jwts.parser().setSigningKey(key).parseClaimsJws(token).getHeader().toString());
 		System.out.println("BODY FROM TOKEN: " + Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody().toString());
-		LinkedHashMap user = (LinkedHashMap) Jwts.parser().setSigningKey(key).parseClaimsJws(token).getHeader().get("user");
-		System.out.println("From encoded user class: " + user.get("userId"));
+		LinkedHashMap user = (LinkedHashMap) Jwts.parser().setSigningKey(key).parseClaimsJws(token);
+		System.out.println("From encoded user class: " + user.toString());
 		try {
+			
 			if(Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody().getIssuer().equals("UxP-Gll")) {
 				if(page < 0) {
 					page = 0;
