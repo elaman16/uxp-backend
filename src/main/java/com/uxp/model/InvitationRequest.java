634b13e91d9663d1d0a192061401e05a70b2bdaf
pyromaticx <krginvest@gmail.com>
2016-06-30 15:20:48 -0700

    asdfae

--- src/main/java/com/uxp/model/InvitationRequest.java
+++ src/main/java/com/uxp/model/InvitationRequest.java
@@ -8,7 +8,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 @Entity
-@Table
+@Table(name="invitations")
 public class InvitationRequest {
 	private static final long serialVersionUID = 1L;
 	@Id
